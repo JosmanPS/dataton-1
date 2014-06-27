6a05b10590689f79a2df17acb1fc0ea50430533a
Oscar <oscarcuellarnevares@hotmail.com>
2014-06-26 20:34:40 -0500

    estilo

--- Dashboard/js/bar_chart.js
+++ Dashboard/js/bar_chart.js
@@ -1,7 +1,7 @@
 createchart = function(filename){
 
 var margin = {top: 20, right: 20, bottom: 30, left: 28},
-    width = 800 - margin.left - margin.right,
+    width = 1000 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
 var x = d3.scale.ordinal()
